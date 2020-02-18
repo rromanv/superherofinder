3a9703548fd0e9555ea6e30551ca9ac5f910c33c
Rodolfo Roman <rromanv@gmail.com>
2020-02-17 23:46:01 -0600

    Home Layout Finish

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,6 +10,7 @@
       />
     </label>
     <div class="flex flex-col flex-wrap justify-center items-center">
+      <PulseLoader v-if="loading" class="mt-4" color="#38B2AC" size="2em" />
       <h1
         v-if="searchItem"
         class="w-full text-center px-4 md:px-16 text-teal-200 font-semibold text-3xl md:text-4xl lg:text-5xl xl:text-6xl"
@@ -18,24 +19,48 @@
           searchItem
         }}"
       </h1>
-      <div class="w-full flex flex-wrap">
-        <Card class="w-1/4 p-2" v-for="n in 10" :key="n" />
+      <div v-if="notFound">
+        <img :src="notFoundImage" alt="not found" />
+      </div>
+      <div v-if="searchItem && heroes.length > 0" class="w-full flex flex-wrap">
+        <Card
+          class="sm:w-1/2 lg:w-1/3 xl:w-1/4 p-2"
+          v-for="hero in heroes"
+          :key="hero.id"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
 import Card from '@/components/Card.vue'
+import notFoundImage from '../assets/notFound.gif'
 
 export default {
   name: 'Home',
   components: {
     Card,
+    PulseLoader,
+  },
+  data() {
+    return {
+      notFoundImage,
+    }
   },
   computed: {
     searchItem() {
       return this.$store.getters.getSearchItem
+    },
+    heroes() {
+      return this.$store.getters.getHeroes
+    },
+    loading() {
+      return this.$store.getters.isLoading
+    },
+    notFound() {
+      return this.$store.getters.getNotFound
     },
   },
   methods: {
