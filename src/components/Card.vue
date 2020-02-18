1d8af33afbb3580383ad3fd67e1975263ae99404
Rodolfo Roman <rromanv@gmail.com>
2020-02-18 00:11:21 -0600

    Home Finish

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="p-2 flex">
-    <router-link to="/" class="w-full">
+  <div class="p-2 flex transform ease-in-out duration-300 hover:scale-110">
+    <router-link :to="`/hero/${hero.id}`" class="w-full">
       <div class="mb-8 px-4">
         <div class="pb-5/6">
           <img
             class="h-full w-full object-cover rounded-lg shadow-md"
-            src="https://via.placeholder.com/800x600.png?text=Hello+There"
+            :src="hero.image.url"
             alt="Hero"
           />
         </div>
@@ -14,13 +14,36 @@
             <h4
               class="mt-1 font-hairline text-3xl text-gray-900 leading-tight truncate"
             >
-              Hero Name
+              {{ hero.name }}
             </h4>
-            <div class="mt-1">Description</div>
+            <div class="mt-1">
+              <span class="font-semibold">Real Name:</span>
+              {{ hero.biography['full-name'] }}
+            </div>
+            <div class="mt-1 font-semibold">
+              Aliases:
+              <span
+                v-for="(alias, index) in hero.biography.aliases"
+                :key="index"
+                class="font-normal"
+                >{{ index > 0 ? ', ' : '' }}{{ alias }}</span
+              >
+            </div>
+            <div class="mt-1">
+              <span class="font-semibold">Publisher:</span>
+              {{ hero.biography.publisher }}
+            </div>
             <div class="flex items-baseline mt-4">
               <span
-                class="inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide"
-                >Something</span
+                class="inline-block text-xs px-2 rounded-full uppercase font-semibold tracking-wide"
+                :class="
+                  hero.biography.alignment === 'good'
+                    ? 'bg-teal-200 text-teal-800'
+                    : hero.biography.alignment === 'bad'
+                    ? 'bg-red-200 text-red-800'
+                    : 'bg-yellow-200 text-yellow-800'
+                "
+                >{{ hero.biography.alignment }}</span
               >
             </div>
           </div>
@@ -33,5 +56,11 @@
 <script>
 export default {
   name: 'Card',
+  props: {
+    hero: {
+      type: Object,
+      default: () => {},
+    },
+  },
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -27,6 +27,7 @@
           class="sm:w-1/2 lg:w-1/3 xl:w-1/4 p-2"
           v-for="hero in heroes"
           :key="hero.id"
+          :hero="hero"
         />
       </div>
     </div>
